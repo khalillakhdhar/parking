3e8aef2dba63b4af8de4fa8db4b4add254c29e4e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-21 10:56:00 +0100

    park

--- src/app/classes/session.ts
+++ src/app/classes/session.ts
@@ -1,5 +1,6 @@
 export class Session {
-    entree:string;
-    sortie:string;
+    entree:any;
+    sortie:any;
     matricule:string;
+    totale:number;
 }
--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -3,6 +3,9 @@ import { SessionService } from '../services/session.service';
 import { PaiementService } from '../services/paiement.service';
 import { Paiement } from '../classes/paiement';
 import { Session } from 'app/classes/session';
+import { Observable } from 'rxjs';
+import { AngularFireStorage } from "@angular/fire/storage";
+import { finalize } from "rxjs/operators";
 
 @Component({
   selector: 'app-icons',
@@ -14,12 +17,17 @@ paiements:Paiement[];
 sessions:Session[];
 session:Session;
 paiement:Paiement;
-  constructor(private sessionService:SessionService,private paiementService:PaiementService) { }
+downloadURL: Observable<string>;
+selectedFile: File = null;
+cv:string;
+fb = "";
+  constructor(private storage: AngularFireStorage,private sessionService:SessionService,private paiementService:PaiementService) { }
 prix:number;
   ngOnInit() {
     this.session=new Session();
     this.paiement=new Paiement();
     this.readpaiement();
+    this.readsession();
   }
 readpaiement()
 {
@@ -47,5 +55,73 @@ this.paiement.prix_heure=this.prix;
 
   });
 }
+readsession()
+{
 
+  this.sessionService.read_Sessions().subscribe(data => {
+
+    this.sessions = data.map(e => {
+      return {
+       id: e.payload.doc.id,
+
+       
+       matricule: e.payload.doc.data()["matricule"],
+       entree: e.payload.doc.data()["entree"],
+       sortie: e.payload.doc.data()["sortie"],
+       totale: e.payload.doc.data()["totale"],
+
+
+
+      };
+    });
+   
+    console.log(this.sessions);
+
+  });
+}
+add()
+{
+  this.session.matricule=this.fb;
+  this.session.entree=Date.now();
+  this.session.totale=this.prix;
+  this.session.sortie="NA"
+let ses=Object.assign({},this.session);
+this.sessionService.create_NewSession(ses);
+
+
+}
+edit(id)
+{
+  this.session.matricule=this.fb;
+  this.session.sortie=Date.now();
+let ses=Object.assign({},this.session);
+this.sessionService.update_Session(id,ses);
+
+
+}
+onFileSelected(event) {
+  var n = Date.now();
+  const file = event.target.files[0];
+  const filePath = `/offre/${n}`;
+  const fileRef = this.storage.ref(filePath);
+  const task = this.storage.upload(`/offre/${n}`, file);
+  task
+    .snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.downloadURL = fileRef.getDownloadURL();
+        this.downloadURL.subscribe((url) => {
+          if (url) {
+            this.fb = url;
+          }
+          console.log(this.fb);
+        });
+      })
+    )
+    .subscribe((url) => {
+      if (url) {
+        console.log(url);
+      }
+    });
+}
 }
